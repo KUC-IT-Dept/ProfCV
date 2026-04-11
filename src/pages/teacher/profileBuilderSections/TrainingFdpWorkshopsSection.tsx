adf60e76a3bd8d78128c01264b7ab40437647e9b
fathmashrn <sherinbt123@gmail.com>
2026-04-11 13:53:08 +0530

    Updated

--- src/pages/teacher/profileBuilderSections/TrainingFdpWorkshopsSection.tsx
+++
@@ -1,7 +0,0 @@
-import { Profile } from './profileBuilderTypes';
-import SectionShell from './SectionShell';
-
-type Props = { profile: Profile; };
-export default function TrainingFdpWorkshopsSection(_props: Props) {
-  return <SectionShell title="TrainingFdpWorkshops" description="TrainingFdpWorkshops details will be added here." />;
-}
